16bfbc77ccafc82cc610d73001c471c2bdcabfd1
Ojas Bansal <ojasbansal.041@gmail.com>
2020-11-11 22:17:20 +0530

    Added Update and Show

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -8,10 +8,39 @@ class InterviewsController < ApplicationController
   end
 
   def create
+    @interview = Interview.new(interview_params)
     
+    if @interview.save
+      redirect_to interviews_path
+    else
+      render 'new'
+    end
   end
 
   def edit
     @interview = Interview.find(params[:id])
   end
+
+  def show
+    @interview = Interview.find(params[:id])
+    @users = @interview.users
+  end
+
+  def update
+    @interview = Interview.find(params[:id])
+    if @interview.update(interview_params)
+      flash[:notice] = "Interview was updated successfully"
+      redirect_to interviews_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(:starttime,:endtime, user_ids: [])
+  end
+
 end
+
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,4 +1,54 @@
+class ModelValidator < ActiveModel::Validator
+  def validate(record)
+    return if record.starttime == nil || record.endtime == nil
+    if record.starttime > record.endtime
+      record.errors[:base] << "Interview Endtime should be after Start time"
+    end
+
+    if record.starttime < Time.now
+      record.errors[:base] << "Interview Must be Scheduled for the future."
+    end
+
+    if !(checkUsersCount record)
+      record.errors[:base] << "Number of users must be greater than 2."
+    end
+
+    if clash record
+      record.errors[:base] << "Some participants are busy in this time slot."
+    end
+  end
+
+  private
+  def checkUsersCount(obj)
+    if obj.users.length >= 2
+      return true
+    else
+      return false
+    end
+  end
+
+  def clash(obj)
+    usersArr = obj.users
+    l = obj.starttime
+    r = obj.endtime
+    usersArr.each do |user|
+      obj2 = user.interviews
+      obj2.each do |interview|
+        if interview.id == obj.id || interview.starttime > r || interview.endtime < l
+          next
+        else
+          return true
+        end
+      end
+    end
+    return false
+  end
+
+end
+
 class Interview < ApplicationRecord
   has_many :interview_users
   has_many :users, through: :interview_users 
+  validates :starttime, :endtime, presence: true
+  validates_with ModelValidator
 end
